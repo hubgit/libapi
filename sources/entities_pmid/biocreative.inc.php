86fc594c02e7134c4ff38fba7659c2fc00cde519
alf <sites@hubmed.org>
2009-06-02 18:36:32 +0100

    added biocreative

---
+++ sources/entities_pmid/biocreative.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+# http://bcms.bioinfo.cnio.es/
+
+function entities_pmid_biocreative($pmid){ 
+  debug($pmid);
+  $http = array('method'=> 'POST', 'content' => xmlrpc_encode_request('Annotations.getAnnotations', $pmid), 'header' => 'Content-Type: text/xml');
+  $data = get_data('http://bcms.bioinfo.cnio.es/xmlrpc/', array(), 'raw', $http);
+  
+  $data = xmlrpc_decode($data);
+     
+  debug($data);
+  
+  if (!is_array($data) || isset($data['faultCode']))
+    return array();
+    
+  $entities = array();
+  $references = array();
+  
+  foreach ($data as $item){
+    foreach ($item['mentions'] as $reference){
+      $references[] = array(
+        'name' => $reference['mention'],
+        'start' => (int) $reference['offset'],
+        'score' => (float) $reference['confidence'],
+        );
+    }
+  }
+  
+  return array($entities, $references);
+}
