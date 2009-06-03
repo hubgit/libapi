906f1362515e047c12d4112b57079e33250c9e2e
alf <sites@hubmed.org>
2009-06-03 16:37:18 +0100

    delicious working

--- sources/bookmarks/delicious.inc.php
+++ sources/bookmarks/delicious.inc.php
@@ -10,11 +10,30 @@ function bookmarks_delicious($q){
   
   debug($json);
     
-  if (!is_object($json) || empty($json))
+  if (!is_array($json) || empty($json))
     return FALSE;
     
   $result = $json[0];
+  $output = array($result->total_posts, array(), array('tags' => (array) $result->top_tags));
+  
+  $xml = get_data('http://feeds.delicious.com/v2/rss/url/' . md5($uri), array(), 'xml');
+  if (!is_object($xml))
+    return $output;
+  
+  $items = array();
+  foreach ($xml->channel->item as $item){
+    $item->registerXPathNamespace('dc', 'http://purl.org/dc/elements/1.1/');
     
-  return array($result->total_posts, $items, array('tags' => (array) $result->top_tags));
+    $items[] = array(
+      'date' => xpath_item($item, 'pubDate'),
+      'title' => xpath_item($item, 'title'),
+      'user' => xpath_item($item, 'dc:creator'),
+      'tags' => xpath_items($item, 'category'),
+    );
+  }
+  
+  $output[1] = $items;
+  
+  return $output;
 }
 
