664fd4b8f01c8596d911a6859968d475345dca52
alf <sites@hubmed.org>
2009-06-03 17:33:55 +0100

    wikipedia search

--- test/citedby.php
+++ test/citedby.php
@@ -3,9 +3,9 @@
 require '../main.inc.php';
 
 $q = array(
-  'doi' => '10.1038/nature05432',
+  //'doi' => '10.1038/nature05432',
   //'doi' => '10.1006/mpev.2001.0963',
-  //'pmid' => 11476639,
+  'pmid' => 11476639,
   );
 
 $api = new API('citedby');
