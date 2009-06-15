2fdf21b144a7469097c7342ffdf68049321333bc
alf <sites@hubmed.org>
2009-06-15 11:16:36 +0100

    added nytimes

--- test/content.php
+++ test/content.php
@@ -3,9 +3,12 @@
 require '../main.inc.php';
 
 $q = array(
-  'title' => 'Google',
+  //'title' => 'Google',
+  'facet' => 'SCIENCE AND TECHNOLOGY',
+  'output' => '../data/nytimes',
   );
 
-$api = new API('content');
+//$api = new API('content');
+$api = new API('content', 'nytimes');
 $responses = $api->all($q);
 debug($responses);
