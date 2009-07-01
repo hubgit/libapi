eeecee834996aa54e0bd7bfbad83b22915838fda
alf <sites@hubmed.org>
2009-07-01 17:58:33 +0100

    added wikipedia categories

--- test/categories.php
+++ test/categories.php
@@ -5,8 +5,9 @@ require '../main.inc.php';
 $q = array(
   'text' => file_get_contents('text/news.txt'), 
   'pmid' => 18464,
+  'title' => array('Google', 'Yahoo'),
 );
 
-$api = new API('categories', 'opencalais');
+$api = new API('categories', 'wikipedia');
 $responses = $api->run($q);
 debug($responses);
