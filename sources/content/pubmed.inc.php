0591f026abb5b61a0604f52a1bd34423dbba144e
aeaton <aeaton@tech2.nature.com>
2009-09-17 06:17:48 -0400

    improved date types in pubmed fetching

--- sources/content/pubmed.inc.php
+++ sources/content/pubmed.inc.php
@@ -8,7 +8,7 @@ function content_pubmed($q){
     
   if (isset($q['output']))
     $output_folder = output_folder($q['output']);
-    
+  
   if (isset($q['from']))
     $from = $q['from'];
   else if ($output_folder && file_exists($output_folder . '/latest'))
@@ -16,58 +16,61 @@ function content_pubmed($q){
   else
     $from = 0;
 
-  $to = date('Y/m/d', time() + 60*60*24); // tomorrow
-   
-  $n = 500;
-  $start = 0;
-  
-  $pubmed = new PubMed();
-  $items = array();
-  
-  $params = array(
-    'mindate' => $from,
-    'maxdate' => $to,
-    'datetype' => 'mdat',
-    );
+  $to = date('Y/m/d', time() + 60*60*24*365*10); // 10 years in future
 
-  $result = $pubmed->search($term, $params);
-  if (!$result)
-    return FALSE;
-   
-  do {
+  $n = 500;
+
+  $items = array();
+
+  foreach (array('edat', 'mdat') as $datetype){ // edat = date added to entrez (pdat = published date), mdat = date modified
+    $start = 0;
+
+    $pubmed = new PubMed();
+
     $params = array(
-      'retmax' => $n,
-      'retstart' => $start,
-      //'sort' => 'pub+date',
+      'mindate' => $from,
+      'maxdate' => $to,
+      'datetype' => $datetype,
       );
-     
-    $xml = $pubmed->fetch(NULL, $params);
-    
-    if (!is_object($xml))
+
+    $result = $pubmed->search($term, $params);
+    if (!$result)
       return FALSE;
+
+    do {
+      $params = array(
+        'retmax' => $n,
+        'retstart' => $start,
+        //'sort' => 'pub+date',
+        );
+     
+      $xml = $pubmed->fetch(NULL, $params);
+    
+      if (!is_object($xml))
+        return FALSE;
       
-    //debug($xml); exit();
+      //debug($xml); exit();
     
-    foreach ($xml->PubmedArticle as $article){
-      if ($output_folder){
+      foreach ($xml->PubmedArticle as $article){
         $id = (int) $article->MedlineCitation->PMID;
-        
-        $out = sprintf('%s/%s.xml', $output_folder, $id);
-        file_put_contents($out, $article->asXML());
-        
-        //$date = current($article->xpath("PubmedData/History/PubMedPubDate[@PubStatus='pubmed']"));
-        //if ((int) $date->Year && (int) $date->Month && (int) $date->Day)
-          //file_put_contents($output_folder . '/latest', sprintf('%d/%d/%d', (int) $date->Year, (int) $date->Month, (int) $date->Day));
+        $status = (string) $article->MedlineCitation['Status'];
+        if ($status == 'In-Data-Review')
+          continue;
+          
+        if ($output_folder){
+          $out = sprintf('%s/%s.xml', $output_folder, $id);
+          file_put_contents($out, $article->asXML());
+        }
+        else
+          $items[$id] = $article;
       }
-      else
-        $items[] = $article;
-    }
   
-    sleep(1);
+      sleep(1);
     
-    $start += $n;
+      $start += $n;
     
-  } while ($start < $pubmed->count);
+    } while ($start < $pubmed->count);
+  }
 
   file_put_contents($output_folder . '/latest', date('Y/m/d'));
   
