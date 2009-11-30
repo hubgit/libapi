27a4d2cb08b64a8efaf8675012a3c0f38692fe4a
alf <sites@hubmed.org>
2009-11-30 18:41:36 +0000

    tests

--- test/biological-entities.php
+++ test/biological-entities.php
@@ -4,13 +4,13 @@ require '../main.inc.php';
 
 $q = array(
   'text' => file_get_contents('text/biology.txt'), 
-  'pmid' => 17906637,
+  //'pmid' => 17906637,
 );
 
 $api = new API('entities', array(
-  'whatizit', 
+  //'whatizit', 
   'gopubmed', 
-  'ihop',
+  //'ihop',
   ));
   
 $responses = $api->run($q);
--- test/entities.php
+++ test/entities.php
@@ -1,12 +1,14 @@
 <?php
 
 require '../main.inc.php';
+$debug = 'PRINT';
 
 $q = array(
+  'file' => 'text/news.txt',
   'text' => file_get_contents('text/news.txt'), 
-  'pmid' => 18464,
+  //'pmid' => 18464,
 );
 
-$api = new API('entities', 'opencalais');
+$api = new API('entities', 'unlock');
 $responses = $api->run($q);
 debug($responses);
