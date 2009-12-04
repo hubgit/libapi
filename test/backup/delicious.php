95669ba822acd2849a745303004dcc869269e929
alf <sites@hubmed.org>
2009-12-04 17:40:27 +0000

    moving to OO

--- test/backup/delicious.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require dirname(__FILE__) . '/../../main.inc.php';
-
-$user = 'hublicious';
-
-$api = new API('content', 'delicious_user');
-$api->run(array('output' => '/delicious/user/' . $user));
-
--- test/geocode.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require '../main.inc.php';
-
-$q = '1600 Amphitheatre Parkway, Mountain View, CA';
-
-$api = new API('geocode');
-$responses = $api->run($q);
-debug($responses);
