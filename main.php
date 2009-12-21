23cc70325863267252239c433f47c469c0fb8dfc
alf <sites@hubmed.org>
2009-12-21 12:40:03 +0000

    not changing include_path any more

--- main.php
+++ main.php
@@ -2,12 +2,6 @@
 
 define('LIBAPI_ROOT', dirname(__FILE__));
 
-set_include_path(implode(PATH_SEPARATOR, array(
-  LIBAPI_ROOT,
-  LIBAPI_ROOT . '/lib',
-  get_include_path()
-  )));
-
 set_time_limit(0);
 
 mb_internal_encoding('UTF-8');
@@ -15,8 +9,8 @@ mb_regex_encoding('UTF-8');
 
 libxml_use_internal_errors(FALSE); // true = hide parsing errors; use libxml_get_errors() to display later.
 
-require 'functions.php';
-require 'Config.php';
+require LIBAPI_ROOT . '/lib/functions.php';
+require LIBAPI_ROOT . '/Config.php';
 
 date_default_timezone_set(Config::get('TIMEZONE'));
 
