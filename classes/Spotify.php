b50ff3fc2a8d7aedaeb444ac44196ff6096faef2
root <sites@hubmed.org>
2010-01-15 06:18:46 -0500

    fixed spotify caching

--- classes/Spotify.php
+++ classes/Spotify.php
@@ -10,8 +10,8 @@ class Spotify extends API {
     
     $cache_dir = $this->get_output_dir('spotify/cache/search/track');
     $cache_file = sprintf('%s/%s.xml', $cache_dir, md5($q));
-    
-    if (file_exists($cache_file)){
+  
+    if (file_exists($cache_file) && ((filemtime($cache_file) - time()) < 60*60*24)){ // use the cache file if it's less than one day old
       $xml = simplexml_load_file($cache_file); 
     }
     else {
@@ -29,11 +29,26 @@ class Spotify extends API {
       return FALSE;
     
     $track = $xml->track[0];
+    $album_href = (string) $track->album['href'];
+    
+    $albums = array();
+    foreach ($xml->track as $track)
+      if ((string) $track->album['href'] == $album_href)
+        $albums[$album_href][(int) $track->{'track-number'}] = (string) $track['href'];
+    
+    $tracks = array();
+    foreach ($albums as $id => $items){
+      ksort($items);
+      foreach ($items as $item)
+        $tracks[] = $item;
+    }
+    
     $album = array(
-      'href' => (string) $track->album['href'],
+      'href' => $album_href,
       'artist' => (string) $track->artist->name,
       'album' => (string) $track->album->name,
       'released' => (string) $track->album->released,
+      'tracks' => implode(' ', $tracks),
       //'territories' => (string) $track->album->availability->territories,
       );    
 
