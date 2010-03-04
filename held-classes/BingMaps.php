e787a93dd31c414e28b07f17c77210391fa338a6
alf <sites@hubmed.org>
2010-03-04 21:44:10 +0000

    improved testing

---
+++ held-classes/BingMaps.php
@@ -0,0 +1,67 @@
+<?php
+
+class BingMaps extends API {
+  public $doc = 'http://msdn.microsoft.com/en-us/library/cc980855.aspx';
+  public $def = 'BING_MAPS';
+  
+  private $server = 'http://dev.virtualearth.net/webservices/v1';
+    
+  function request($query, $n = 10){
+    return array('request' => array(
+      'Credentials' => array('ApplicationId' => Config::get('BING_MAPS')),
+      'Query' => $query,
+      'Options' => array('Count' => $n),
+    ));
+  }
+
+  function geocode($text){
+    $client = new SoapClient($this->server . '/geocodeservice/geocodeservice.svc?wsdl');
+    $request = $this->request($text, 1);
+    $result = $client->Geocode($request);
+   
+    debug($result); //exit();
+
+    if (!is_object($result) || $result->GeocodeResult->ResponseSummary->StatusCode != 'Success')
+      return FALSE;
+
+    $data = $result->GeocodeResult->Results->GeocodeResult;
+    if (!is_array($data->Locations->GeocodeLocation))
+      $data->Locations->GeocodeLocation = array($data->Locations->GeocodeLocation);
+    $location = $data->Locations->GeocodeLocation[0];
+
+    return array(
+      'address' => $data->Address->FormattedAddress, 
+      'lat' => (float) $location->Latitude, 
+      'lng' => (float) $location->Longitude,
+      'raw' => $data,
+      );
+  }
+  
+  function search($args){
+    $this->validate($args, 'text', array('n' => 10)); extract($args);
+        
+    $client = new SoapClient($this->server . '/searchservice/searchservice.svc?wsdl');
+    $request = $this->request($text, $n);
+    $result = $client->Search($request);
+        
+    if (!is_object($result) || $result->SearchResult->ResponseSummary->StatusCode != 'Success')
+      return FALSE;
+      
+    //debug($result);
+    
+    return array($result->SearchResult->ResultSets->SearchResultSet->Results->SearchResultBase, array('raw' => $result));
+  }
+  
+  function parse($args){
+    $args['n'] = 1;
+    $result = $this->search($args);
+    
+    if (!is_array($result->SearchResult->ResultSets->SearchResultSet))
+      $result->SearchResult->ResultSets->SearchResultSet = array($result->SearchResult->ResultSets->SearchResultSet);
+      
+    //debug($result->SearchResult->ResultSets->SearchResultSet);
+    
+    $parsed = $result->SearchResult->ResultSets->SearchResultSet[0]->Parse;
+    return array('keyword' => $parsed->Keyword, 'address' => $parsed->Address->FormattedAddress);
+  }
+}
