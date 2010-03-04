ce56e549af2baa90fe18bec28f0fd8ed6e55f9de
root <sites@hubmed.org>
2010-03-04 16:30:17 -0500

    added O lib

---
+++ lib/O.php
@@ -0,0 +1,12 @@
+<?php
+
+class O {
+  static function raw($input){
+    print $input;
+  }
+  
+  static function html($input){
+    $input = mb_convert_encoding($input, 'UTF-8', mb_detect_encoding($input));
+    print htmlspecialchars((string) $input, ENT_QUOTES, 'UTF-8'); // FIXME: filter_var + FILTER_SANITIZE_SPECIAL_CHARS?
+  }
+}
