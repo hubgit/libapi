4327a6821d5ae3793ee5cfab48a4841d90d37c69
alf <sites@hubmed.org>
2010-03-19 15:10:41 +0000

    made _dir functions static

--- API.php
+++ API.php
@@ -125,7 +125,7 @@ class API {
     $this->http_info = array(curl_getinfo($curl));
 
     debug('Status: ' . $this->http_status);  
-    debug($data);
+    //debug($data);
     
     //file_put_contents(sys_get_temp_dir() . '/curl.xml', $data);
     
@@ -207,7 +207,7 @@ class API {
     $this->http_headers[] = $item;
   }
   
-  function get_input_dir($dir = ''){
+  static function get_input_dir($dir = ''){
     if (strpos($dir, '/') !== 0) // path doesn't start with '/', so treat as relative to DATA_DIR
       $dir = Config::get('DATA_DIR') . '/' . $dir;
       
@@ -217,7 +217,7 @@ class API {
     return $dir;
   }
 
-  function get_output_dir($dir = ''){
+  static function get_output_dir($dir = ''){
     #$dir = preg_replace('/[^a-z0-9\(\)\_\-\+ ]/i', '_', $dir); // FIXME: proper sanitising
     
     if (strpos($dir, '/') !== 0) // path doesn't start with '/', so treat as relative to DATA_DIR
--- classes/ChemSpider.php
+++ classes/ChemSpider.php
@@ -14,6 +14,77 @@ class ChemSpider extends API {
       return (int) $xml;
     }
     
+    function InChIToCSID($inchi){
+      $xml = $this->get_data('http://www.chemspider.com/InChI.asmx/InChIToCSID', array('inchi' => $inchi), 'xml');
+
+      debug($xml);
+      if (!is_object($xml))
+        return FALSE;
+
+      return (int) $xml;
+    }
+    
+    function CSID2ExtRefs($args){
+      $this->validate($args, 'csid', array('datasources' => array('Wikipedia'))); extract($args);      
+
+      $params = array(
+        'csid' => $csid,
+        'token' => Config::get('CHEMSPIDER'),
+        'datasources' => implode(',', $datasources),
+        );
+        
+      $xml = $this->get_data('http://www.chemspider.com/Search.asmx/CSID2ExtRefs', $params, 'xml');
+
+      debug($xml);
+      if (!is_object($xml))
+        return FALSE;
+
+      $items = array();
+      foreach ((array) $xml->ExtRef as $key => $value)
+        $items[$key] = (string) $value;
+      
+      return $items;
+    }
+    
+    function GetStructureSynonyms($mol){
+      $params = array(
+        'mol' => $mol,
+        'token' => Config::get('CHEMSPIDER'),
+        );
+        
+      $xml = $this->get_data('http://www.chemspider.com/Synonyms.asmx/GetStructureSynonyms', $params, 'xml');
+      debug($xml); exit();
+      if (!is_object($xml))
+        return FALSE;
+      
+      $items = array();
+      foreach ($xml->string as $item)
+        $items[] = (string) $item;
+        
+      return $items;
+    }
+    
+    function search($args){
+      $this->validate($args, 'query'); extract($args);  
+          
+      $params = array(
+        'query' => $query,
+        'token' => Config::get('CHEMSPIDER'),
+        );
+        
+      $xml = $this->get_data('http://www.chemspider.com/Search.asmx/SimpleSearch', $params, 'xml');
+
+      debug($xml);
+      if (!is_object($xml))
+        return FALSE;
+
+      $items = array();
+      foreach ($xml->int as $value)
+        $items[] = (int) $value;
+      
+      return $items;
+    }
+    
     function image($csid){
       debug('CSID: ' . $csid);
       $params = array(
