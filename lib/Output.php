30d6f761daa375dbe3f51e13cc21193812a1488e
hubgit <sites@hubmed.org>
2010-04-17 13:25:03 +0100

    added h function, updated PubMed

---
+++ lib/Output.php
@@ -0,0 +1,15 @@
+<?php
+
+class Output {
+  static function raw($input){
+    print $input;
+  }
+  
+  static function html($input){
+  if (is_object($input))
+    debug($input);
+    $input = mb_convert_encoding($input, 'UTF-8', mb_detect_encoding($input));
+    print htmlspecialchars((string) $input, ENT_QUOTES, 'UTF-8'); // FIXME: filter_var + FILTER_SANITIZE_SPECIAL_CHARS?
+  }
+}
+
